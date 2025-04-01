327d4d7a0d61fc172bff5b0d4b45580eea064028
Shingo Tamura <shingo206@mac.com>
2025-04-01 23:57:14 +0900

    Implement Search function

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,2 +1,32 @@
-const Page = () => <p>Invoices Page</p>
+import {lusitana} from '@/app/ui/fonts';
+import {CreateInvoice} from '@/app/ui/invoices/buttons';
+import Search from '@/app/ui/search';
+
+type Props = Promise<{
+    query?: string;
+    page?: string;
+}>;
+const Page = async (props: Props) => {
+    const searchParams = await props.searchParams;
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page) || 1;
+
+    return (
+        <div className={'w-full'}>
+            <div className='flex w-full place-content-center'>
+                <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+            </div>
+            <div className={`mt-4 flex place-content-center gap-2 md:mt-8`}>
+                <Search placeholder={'Search Invoices...'}/>
+                <CreateInvoice/>
+            </div>
+            <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton/>}>
+                <Table query={query} currentPage={currentPage}/>
+            </Suspense>
+            <div className={`mt-5 flex w-full justify-center`}>
+                {/*<Pagination totalPages={totalPages}/>*/}
+            </div>
+        </div>
+    );
+};
 export default Page;
