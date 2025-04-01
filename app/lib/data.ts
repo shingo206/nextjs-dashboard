114ac4fd34c06835088c86c239e996ba04554773
Shingo Tamura <shingo206@mac.com>
2025-04-01 20:33:36 +0900

    Complete Chapter 8

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,10 +9,10 @@ export const fetchRevenue = async () => {
         // Artificially delay a response for demo purposes.
         // Don't do this in production :)
 
-        // console.log('Fetching revenue data...');
-        // await new Promise((resolve) => setTimeout(resolve, 3000));
+        console.log('Fetching revenue data...');
+        await new Promise((resolve) => setTimeout(resolve, 3000));
 
-        // console.log('Data fetch completed after 3 seconds.');
+        console.log('Data fetch completed after 3 seconds.');
 
         return await sql<Revenue[]>`SELECT *
                                     FROM revenue`;
